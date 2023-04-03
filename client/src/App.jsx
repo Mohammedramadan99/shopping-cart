2dfc5d9de5303a151c6ed438e789f45293b4fe8b
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-04-03 02:44:47 +0200

    Add Home page and the layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "../pages/home/Home";
 function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
 
-  return (
-    <div>
-      Hello world!
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
