e8fc82b6f550bfe9735da689ea3a6e97d82ae618
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-04-04 15:55:26 +0200

    Add Login page

--- client/src/pages/register/Signup.jsx
+++ client/src/pages/register/Signup.jsx
@@ -1,7 +1,59 @@
-import React from "react";
-
-function Signup() {
-  return <div>Signup</div>;
+import React, { useState } from "react";
+import axios from "axios";
+function SignUp() {
+  const [file, setFile] = useState(null);
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    img: "",
+  });
+  const handleChange = (e) => {
+    setUser((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    // Send the POST request to the backend
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3001/api/auth/login",
+        user
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="SignUp">
+      <div className="container">
+        <form className="form" onSubmit={submitHandler}>
+          <div className="item">
+            <label>email</label>
+            <input
+              name="email"
+              type="text"
+              placeholder="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="item">
+            <label>password</label>
+            <input
+              name="password"
+              type="text"
+              placeholder="password"
+              onChange={handleChange}
+            />
+          </div>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Signup;
+export default SignUp;
